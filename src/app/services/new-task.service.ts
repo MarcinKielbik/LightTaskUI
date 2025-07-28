e84747abb0eedee76b25218e43391e03d9f5274e
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-07-28 12:43:51 +0200

    chore: create NewTaskService

--- src/app/services/new-task.service.ts
+++ src/app/services/new-task.service.ts
@@ -1,5 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+export interface Task {
+  id?: number;
+  title: string;
+  description: string;
+  completed: boolean;
+}
 
 
 @Injectable({
@@ -7,8 +15,13 @@ import { Injectable } from '@angular/core';
 })
 
 
-
 export class NewTaskService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/tasks';
+
+  constructor(private http: HttpClient) { }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task);
+  }
 }
